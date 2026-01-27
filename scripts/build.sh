828bc12666edcfa8077eb84b6fac712464cb07db
luzhixing12345 <luzhixing12345@163.com>
2026-01-27 09:06:42 +0800

    docs: update debian deb pkg build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,18 +8,18 @@ make release
 
 mkdir -p "${PACKAGE_DIR}"
 mkdir -p "${PACKAGE_DIR}/etc/kperf"
-mkdir -p "${PACKAGE_DIR}/usr/local/bin"
-mkdir -p "${PACKAGE_DIR}/debian"
-cp build/kperf "${PACKAGE_DIR}/usr/local/bin/"
+mkdir -p "${PACKAGE_DIR}/usr/bin"
+mkdir -p "${PACKAGE_DIR}/DEBIAN"
+cp build/kperf "${PACKAGE_DIR}/usr/bin/"
 cp assets/* "${PACKAGE_DIR}/etc/kperf/"
-cp debian/* "${PACKAGE_DIR}/debian/"
+cp scripts/control "${PACKAGE_DIR}/DEBIAN/"
 
-VERSION=$(grep "^Version:" "${PACKAGE_DIR}/debian/control" | awk '{print $2}')
+VERSION=$(grep "^Version:" "${PACKAGE_DIR}/DEBIAN/control" | awk '{print $2}')
 OUTPUT_FILE="${PACKAGE_NAME}_${VERSION}_amd64.deb"
 
 rm -f "${OUTPUT_FILE}"
 
-chmod 755 "${PACKAGE_DIR}/usr/local/bin/kperf"
+chmod 755 "${PACKAGE_DIR}/usr/bin/kperf"
 chmod 644 "${PACKAGE_DIR}/etc/kperf/"*
 
 dpkg-deb --build "${PACKAGE_DIR}" "${OUTPUT_FILE}"
